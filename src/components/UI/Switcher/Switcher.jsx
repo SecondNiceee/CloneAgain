77cc2b178e26db573cc1fb9ff99f451ee3c560b3
SecondNiceee <col1596321@gmail.com>
2024-04-10 08:39:59 +0300

    gg

--- src/components/UI/Switcher/Switcher.jsx
+++ src/components/UI/Switcher/Switcher.jsx
@@ -1,11 +1,12 @@
 import React, { memo } from 'react';
 import cl from './Switcher.module.css'
-import circle from '../../../images/icons/circle.png'
+import circle from '../../../images/icons/Circle.svg'
 const Switcher = ({className , isEnable, setEnable}) => {
     return (
         <div onClick={() => setEnable(!isEnable)} className = { [className ? [cl.Switcher , className].join(' ') : cl.Switcher , isEnable ? cl.active : '' ].join(' ')} >
-                <img className = {isEnable ? [cl.circle , cl.active].join(' ') : cl.circle} src={circle} alt="" />
+                <div className = {isEnable ? [cl.circle , cl.active].join(' ') : cl.circle}>
 
+                </div>
         </div>
     );
 };
